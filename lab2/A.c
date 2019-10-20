f0fc1c8bd5a301953ea6c08c90e43713a576cbfb
pprettysimpple <andrianov.kirill2010@yandex.ru>
2019-10-20 23:15:16 +0300

    add final hw4, fairly-full lab2

--- lab2/A.c
+++ lab2/A.c
@@ -4,17 +4,49 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <memory.h>
 
-void sort(int *a, int l, int r) {
-    if (l >= r) return;
-    int i = l, j = r;
-    while (i < j) {
+void sort(int i, int i1, int *pInt);
 
+signed main() {
+    int n;
+    scanf("%d", &n);
+    int *a = malloc(n * sizeof(int));
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &a[i]);
+    }
+    sort(0, n - 1, a);
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", a[i]);
     }
 }
 
-signed main() {
-    int n; scanf("%d", &n);
-    int *a = malloc(n * sizeof(int));
-    sort(a, 0, n - 1);
+void sort(int l, int r, int *a) {
+    if (l >= r) {
+        return;
+    }
+    int m = (l + r) / 2;
+    sort(l, m, a);
+    sort(m + 1, r, a);
+    int *cur = malloc((r - l + 1) * sizeof(int));
+    int p1 = l, p2 = m + 1;
+    while (p1 <= m && p2 <= r) {
+        if (a[p1] <= a[p2]) {
+            *cur = a[p1++];
+        } else {
+            *cur = a[p2++];
+        }
+        cur++;
+    }
+    while (p1 <= m) {
+        *cur = a[p1++];
+        cur++;
+    }
+    while (p2 <= r) {
+        *cur = a[p2++];
+        cur++;
+    }
+    cur -= (r - l + 1);
+    memcpy(a + l, cur, sizeof(int) * (r - l + 1));
+    free(cur);
 }
